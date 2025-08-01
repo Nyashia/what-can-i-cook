027828ff1dc62bb8e81527b8d18b04434335042c
Nyashia <NyashiaCarrington0@gmail.com>
2025-08-01 12:33:27 -0400

    Initial commit with API integration and .gitignore

--- app.py
+++ app.py
@@ -1,17 +1,43 @@
 from flask import Flask, render_template, request
 import requests
+import os
+
+load_dotenv()  #loading environment variables from .env file
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('index.html')
+    recipes = []
+    if request.method == 'POST':
+        ingredients = request.form.get('ingredients')
+        #PLaceholder for API call
+        recipes = get_recipes(ingredients)
+    return render_template('index.html', recipes=recipes)
 
-@app.route('/search', methods=['POST'])
-def search():
-    ingredients = request.form['ingredients']
-    # For now, just pass ingredients back to the template
-    return render_template('results.html', ingredients=ingredients)
+def get_recipes(ingredients):
+    api_key = os.environ.get('SPOONACULAR_API_KEY')
+    if not api_key:
+        return []
+
+    url = 'https://api.spoonacular.com/recipes/findByIngredients'
+    params = {
+        'ingredients': ingredients,
+        'number': 5,
+        'apiKey': api_key
+    }
+
+    response = requests.get(url, params=params)
+
+    if response.status_code != 200:
+        return []
+
+    data = response.json()
+    recipes = [{'title': item['title']} for item in data]
+
+    return recipes
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
